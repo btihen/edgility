6345e8fb88741130be49c61f9c99c7698e8d5444
Bill Tihen <btihen@las.ch>
2016-11-09 19:49:15 +0100

    change to link rbenv-vars file

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,8 @@ set :repo_url, 'git@github.com:btihen:edgility.git'
 
 set :deploy_to, '/home/deploy/edgility'
 
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .rbenv-vars}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
